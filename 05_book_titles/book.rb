ee2dce6c6fab9d82eb6a0dcd483f1914a6843c68
richjdsmith <rich@richjdsmith.ca>
2017-01-22 16:04:48 -0800

    Completed test requirements

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,29 @@
 class Book
-# write your code here
+
+    def title= (title)
+
+        #split into separate array items
+        title_words = title.split
+        #list of all small words to test for (Note excluded "I")
+        small = ["and", "in", "the", "of", "a", "an"]
+
+        #capitalize all words which are not small words
+        title_words.each do |word|
+            if not small.include?(word)
+                word.capitalize!
+            end
+        end
+        #Always capitalize the first word, regardless if it is a small word.
+        title_words[0].capitalize!
+
+        #create instance variable while joining our previously split words.
+        @title = title_words.join(' ')
+    end
+
+    #setting title to our rebuilt string. 
+    def title
+        @title
+    end
+
 end
+
